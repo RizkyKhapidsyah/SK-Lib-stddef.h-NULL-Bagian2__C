a573239f27afee54b23098aea0683b7041bc047e
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-01-11 08:33:01 +0700

    Add project files.

---
+++ SK-Lib-stddef.h-NULL-Bagian2__C/Source.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+#include <conio.h>
+
+/*
+    Source by GeeksForGeeks
+    Modified For Learn by RK
+    I.D.E : VS2019
+*/
+
+int main() {
+	int* num = NULL;
+    _getch();
+	return 0;
+}
